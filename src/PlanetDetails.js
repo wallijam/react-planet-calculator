83ccba280bbbf2ac4406a05fc1f716a69365ef5e
James Wallington <wallijam@mail.gvsu.edu>
2023-05-07 21:27:57 +0000

    Update PlanetDetails.js and SolarSystem.js

--- src/PlanetDetails.js
+++ src/PlanetDetails.js
@@ -1,50 +1,24 @@
+import React from "react";
 import { Box, Typography } from "@mui/material";
-import { useState } from "react";
 
-function PlanetDetails({ planet, onClose }) {
-  const [showMoreInfo, setShowMoreInfo] = useState(false);
-
-  const handleMoreInfoClick = () => {
-    setShowMoreInfo(true);
-  };
-
+export function PlanetDetails({ planet }) {
   return (
-    <div className="planet-details-overlay">
-      <Box sx={{ position: "absolute", top: 20, right: 20 }}>
-        <button onClick={onClose}>Close</button>
-      </Box>
-      <Box
-        sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-      >
-        <Typography variant="h6">{planet.name}</Typography>
-        <Typography variant="body1">{planet.description}</Typography>
-        <Typography variant="body1">Mass: {planet.mass} kg</Typography>
-        <Typography variant="body1">Climate: {planet.climate}</Typography>
-        <Typography variant="body1">
-          Temperature: {planet.temperature}°C
-        </Typography>
-        {!showMoreInfo && (
-          <Typography
-            variant="body1"
-            sx={{ mt: 1, textDecoration: "underline", cursor: "pointer" }}
-            onClick={handleMoreInfoClick}
-          >
-            More info
-          </Typography>
-        )}
-        {showMoreInfo && (
-          <div>
-            <Typography variant="body1">
-              Oxygen levels: {planet.oxygen}
-            </Typography>
-            <Typography variant="body1">
-              Gravity: {planet.gravity} m/s²
-            </Typography>
-          </div>
-        )}
-      </Box>
-    </div>
+    <Box>
+      <Typography variant="h6" align="center" gutterBottom>
+        {planet.name}
+      </Typography>
+      <Typography variant="body1" align="center" gutterBottom>
+        Mass: {planet.mass}
+      </Typography>
+      <Typography variant="body1" align="center" gutterBottom>
+        Temperature: {planet.temperature}
+      </Typography>
+      <Typography variant="body1" align="center" gutterBottom>
+        Climate: {planet.climate}
+      </Typography>
+      <Typography variant="body1" align="center">
+        Oxygen Level: {planet.oxygenLevel}
+      </Typography>
+    </Box>
   );
 }
-
-export default PlanetDetails;
